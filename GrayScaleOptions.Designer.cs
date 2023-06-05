e984fa2d912383781c49bc56c976a084990d4909
luvairo <luvairo@mail.ru>
2023-06-06 00:35:23 +0500

    Переработан фильтр серого цвета

--- GrayScaleOptions.Designer.cs
+++ GrayScaleOptions.Designer.cs
@@ -28,35 +28,185 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            groupBox1 = new GroupBox();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            label5 = new Label();
+            label3 = new Label();
             label1 = new Label();
+            redWeight = new TextBox();
+            greenWeight = new TextBox();
+            blueWeight = new TextBox();
+            redErrorProvider = new ErrorProvider(components);
+            greenErrorProvider = new ErrorProvider(components);
+            blueErrorProvider = new ErrorProvider(components);
+            redToolTip = new ToolTip(components);
+            groupBox1.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)redErrorProvider).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)greenErrorProvider).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)blueErrorProvider).BeginInit();
             SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(tableLayoutPanel1);
+            groupBox1.Dock = DockStyle.Fill;
+            groupBox1.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            groupBox1.Location = new Point(0, 0);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(484, 261);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "GrayScale options";
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(label5, 0, 2);
+            tableLayoutPanel1.Controls.Add(label3, 0, 1);
+            tableLayoutPanel1.Controls.Add(label1, 0, 0);
+            tableLayoutPanel1.Controls.Add(redWeight, 1, 0);
+            tableLayoutPanel1.Controls.Add(greenWeight, 1, 1);
+            tableLayoutPanel1.Controls.Add(blueWeight, 1, 2);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(3, 21);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 3;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.Size = new Size(478, 237);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Dock = DockStyle.Fill;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(3, 156);
+            label5.Name = "label5";
+            label5.Size = new Size(233, 81);
+            label5.TabIndex = 4;
+            label5.Text = "Blue weight:";
+            label5.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Dock = DockStyle.Fill;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(3, 78);
+            label3.Name = "label3";
+            label3.Size = new Size(233, 78);
+            label3.TabIndex = 2;
+            label3.Text = "Green weight:";
+            label3.TextAlign = ContentAlignment.MiddleRight;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Dock = DockStyle.Fill;
             label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(0, 0);
+            label1.Location = new Point(3, 0);
             label1.Name = "label1";
-            label1.Size = new Size(188, 21);
+            label1.Size = new Size(233, 78);
             label1.TabIndex = 0;
-            label1.Text = "No options for this filter :(";
-            label1.TextAlign = ContentAlignment.MiddleCenter;
+            label1.Text = "Red weight:";
+            label1.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // redWeight
+            // 
+            redWeight.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            redWeight.Location = new Point(242, 8);
+            redWeight.Margin = new Padding(3, 8, 3, 3);
+            redWeight.MaxLength = 10;
+            redWeight.Name = "redWeight";
+            redWeight.Size = new Size(100, 29);
+            redWeight.TabIndex = 5;
+            redWeight.Text = "0,2126";
+            redToolTip.SetToolTip(redWeight, "default value: 0,2126");
+            redWeight.TextChanged += RedWeightTextBoxChanged;
+            // 
+            // greenWeight
+            // 
+            greenWeight.Dock = DockStyle.Left;
+            greenWeight.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            greenWeight.Location = new Point(242, 86);
+            greenWeight.Margin = new Padding(3, 8, 3, 3);
+            greenWeight.MaxLength = 10;
+            greenWeight.Name = "greenWeight";
+            greenWeight.Size = new Size(100, 29);
+            greenWeight.TabIndex = 6;
+            greenWeight.Text = "0,7152";
+            redToolTip.SetToolTip(greenWeight, "default value: 0,7152");
+            greenWeight.TextChanged += GreenWeightTextBoxChanged;
+            // 
+            // blueWeight
+            // 
+            blueWeight.BackColor = SystemColors.Window;
+            blueWeight.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            blueWeight.Location = new Point(242, 164);
+            blueWeight.Margin = new Padding(3, 8, 3, 3);
+            blueWeight.MaxLength = 10;
+            blueWeight.Name = "blueWeight";
+            blueWeight.Size = new Size(100, 29);
+            blueWeight.TabIndex = 7;
+            blueWeight.Text = "0,0722";
+            redToolTip.SetToolTip(blueWeight, "default value: 0,0722");
+            blueWeight.TextChanged += BlueWeightTextBoxChanged;
+            // 
+            // redErrorProvider
+            // 
+            redErrorProvider.BlinkRate = 800;
+            redErrorProvider.ContainerControl = this;
+            // 
+            // greenErrorProvider
+            // 
+            greenErrorProvider.BlinkRate = 800;
+            greenErrorProvider.ContainerControl = this;
+            // 
+            // blueErrorProvider
+            // 
+            blueErrorProvider.BlinkRate = 800;
+            blueErrorProvider.ContainerControl = this;
+            // 
+            // redToolTip
+            // 
+            redToolTip.AutomaticDelay = 100;
             // 
             // GrayScaleOptions
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(label1);
+            ClientSize = new Size(484, 261);
+            Controls.Add(groupBox1);
             Name = "GrayScaleOptions";
             Text = "GrayScaleOptions";
+            groupBox1.ResumeLayout(false);
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)redErrorProvider).EndInit();
+            ((System.ComponentModel.ISupportInitialize)greenErrorProvider).EndInit();
+            ((System.ComponentModel.ISupportInitialize)blueErrorProvider).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
+        private GroupBox groupBox1;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Label label5;
+        private Label label3;
         private Label label1;
+        private TextBox redWeight;
+        private TextBox greenWeight;
+        private TextBox blueWeight;
+        private ErrorProvider redErrorProvider;
+        private ErrorProvider greenErrorProvider;
+        private ErrorProvider blueErrorProvider;
+        private ToolTip redToolTip;
     }
 }
--- GrayScaleOptions.cs
+++ GrayScaleOptions.cs
@@ -2,32 +2,87 @@
 {
     public partial class GrayScaleOptions : Form, IFilter
     {
+        private float? rBuffer, gBuffer, bBuffer;
+
         public GrayScaleOptions() => InitializeComponent();
 
         public Image GetFilteredImage(Image image)
         {
+            if (redErrorProvider.GetError(redWeight) != string.Empty
+                || greenErrorProvider.GetError(greenWeight) != string.Empty
+                || blueErrorProvider.GetError(blueWeight) != string.Empty)
+            {
+                MessageBox.Show(
+                    "Bad filter's parameters",
+                    "Filter error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+
+                return image;
+            }
+
+            var (rWeight, gWeight, bWeight) =
+                (float.Parse(redWeight.Text),
+                float.Parse(greenWeight.Text),
+                float.Parse(blueWeight.Text));
+
+            if (rBuffer == null && gBuffer == null && bBuffer == null)
+                (rBuffer, gBuffer, bBuffer) = (rWeight, gWeight, bWeight);
+            else if (rWeight == rBuffer && gWeight == gBuffer && bWeight == bBuffer)
+                return image;
+
             var input = new Bitmap(image);
             var bitmap = new Bitmap(input.Width, input.Height);
 
-            for (var j = 0; j < bitmap.Height; j++)
-                for (var i = 0; i < bitmap.Width; i++)
+            for (var i = 0; i < bitmap.Height; i++)
+                for (var j = 0; j < bitmap.Width; j++)
                 {
-                    var pixel = input.GetPixel(i, j).ToArgb();
+                    var pixel = input.GetPixel(j, i).ToArgb();
 
-                    var r = (float)((pixel & 0x00FF0000) >> 16);
-                    var g = (float)((pixel & 0x0000FF00) >> 8);
-                    var b = (float)(pixel & 0x000000FF);
+                    var alpha = (float)((pixel & 0xFF000000) >> 24);
+                    var red = (float)((pixel & 0x00FF0000) >> 16);
+                    var green = (float)((pixel & 0x0000FF00) >> 8);
+                    var blue = (float)(pixel & 0x000000FF);
 
-                    r = g = b = (r + g + b) / 3.0f;
+                    red = green = blue = rWeight * red + gWeight * green + bWeight * blue;
 
-                    var filteredPixel = 0xFF000000 | ((uint)r << 16) | ((uint)g << 8) | ((uint)b);
-                    bitmap.SetPixel(i, j, Color.FromArgb((int)filteredPixel));
+                    var filteredPixel = 0x00000000 |
+                        ((uint)alpha << 24) | ((uint)red << 16) | ((uint)green << 8) | ((uint)blue);
+                    bitmap.SetPixel(j, i, Color.FromArgb((int)filteredPixel));
                 }
 
             GC.Collect();
             GC.WaitForPendingFinalizers();
 
+            (rBuffer, gBuffer, bBuffer) = (rWeight, gWeight, bWeight);
+
             return bitmap;
+        }
+
+        private void RedWeightTextBoxChanged(object sender, EventArgs e)
+            => ValidateFloatInput((TextBox)sender, redErrorProvider);
+
+        private void GreenWeightTextBoxChanged(object sender, EventArgs e)
+            => ValidateFloatInput((TextBox)sender, greenErrorProvider);
+
+        private void BlueWeightTextBoxChanged(object sender, EventArgs e)
+            => ValidateFloatInput((TextBox)sender, blueErrorProvider);
+
+        private void ValidateFloatInput(TextBox sender, ErrorProvider provider)
+        {
+            var input = sender.Text;
+
+            if (float.TryParse(input, out float value))
+            {
+                if (value < 0 || value > 1)
+                    provider.SetError(sender, "Value must be in [0, 1]");
+                else
+                    provider.Clear();
+
+                return;
+            }
+
+            provider.SetError(sender, "Invalid number format, use comma instead of dout");
         }
     }
 }
